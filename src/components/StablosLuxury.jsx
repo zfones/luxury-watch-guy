c5ff653f0f0f7003cb735d7567da521fd892e081
zfones <zachfones@gmail.com>
2025-01-27 13:31:01 -0500

    Add main React component

---
+++ src/components/StablosLuxury.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Watch, Diamond, Shield, CreditCard, Star, Menu, Search, ShoppingBag } from 'lucide-react';
+
+const StablosLuxury = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  return (
+    <div className="min-h-screen bg-black text-neutral-100">
+      {/* Component content as before */}
+    </div>
+  );
+};
+
+export default StablosLuxury;
