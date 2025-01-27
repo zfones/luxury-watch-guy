29cf8341a8060ba843b2094f4bc29724554f884e
zfones <zachfones@gmail.com>
2025-01-27 13:33:24 -0500

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StablosLuxury from './components/StablosLuxury';
+
+function App() {
+  return (
+    <div className="App">
+      <StablosLuxury />
+    </div>
+  );
+}
+
+export default App;
